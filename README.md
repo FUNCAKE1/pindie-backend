9eee5c8447f009ab5377f9c83edc06a065c72150
FUNCAKE1 <149552929+FUNCAKE1@users.noreply.github.com>
2024-05-26 19:23:44 +0700

    Финальное ревью

---
+++ README.md
@@ -0,0 +1,5 @@
+Ссылка на репозиторий с фронтендом: https://github.com/FUNCAKE1/pindie-frontend/pull/1
+
+Ссылка на домен фронтенда: https://pindie-front.nomoredomainswork.ru/
+
+IP-адрес: 158.160.167.80
---
+++ routes/games.js
@@ -0,0 +1,50 @@
+const gamesRouter = require("express").Router();
+
+const { findAllGames } = require("../middlewares/games");
+const { sendAllGames } = require("../controllers/games");
+
+const { createGame } = require("../middlewares/games");
+const { sendGameCreated } = require("../controllers/games");
+const { findGameById } = require("../middlewares/games");
+const { sendGameById } = require("../controllers/games");
+const { sendGameUpdated } = require("../controllers/games");
+const { updateGame } = require("../middlewares/games");
+const { deleteGame } = require("../middlewares/games");
+const { sendGameDeleted } = require("../controllers/games");
+const { checkIsGameExists } = require("../middlewares/games");
+const { checkIfCategoriesAvaliable } = require("../middlewares/games");
+const { checkEmptyFields } = require("../middlewares/games");
+const { checkIfUsersAreSafe } = require("../middlewares/games");
+const { checkAuth } = require("../middlewares/auth.js");
+const { checkIsVoteRequest } = require("../middlewares/games.js");
+
+gamesRouter.get("/games", findAllGames, sendAllGames);
+
+gamesRouter.post(
+  "/games",
+  findAllGames,
+  checkIsGameExists,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  createGame,
+  sendGameCreated
+);
+
+gamesRouter.get("/games/:id", findGameById, sendGameById);
+
+gamesRouter.put(
+  "/games/:id",
+  findGameById,
+  checkIsVoteRequest,
+  checkIfUsersAreSafe,
+  checkIfCategoriesAvaliable,
+  checkEmptyFields,
+  checkAuth,
+  updateGame,
+  sendGameUpdated
+);
+
+gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
+
+module.exports = gamesRouter;
